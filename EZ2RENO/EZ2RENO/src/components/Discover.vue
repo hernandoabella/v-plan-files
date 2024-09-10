cd8c268378edf1275c1eb0f2fca1458de58e4ac9
Hernando Abella <hernandoabella@gmail.com>
2024-09-09 22:07:27 -0500

    Create Discover.vue

---
+++ EZ2RENO/EZ2RENO/src/components/Discover.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-5">
+    <h2 class="text-center fw-bolder">Discover Your Dream Home</h2>
+    <p class="text-center">
+      At EZ2 Reno, our mission is to build homes that inspire and delight. Photo
+      Gallery.
+    </p>
+
+    <div>
+      <div class="w-100 d-flex w-100 d-flex justify-content-center">
+        <img src="/public/image 139.png" alt="kitchen" />
+      </div>
+      <div class="d-flex w-50 mx-auto justify-content-center gap-2 mt-2">
+        <div>
+          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        </div>
+        <div>
+          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        </div>
+        <div>
+          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        </div>
+        <div>
+          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        </div>
+        <div>
+          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
