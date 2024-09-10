878a44e5436f24330664abc638358d51f090cadc
Hernando Abella <hernandoabella@gmail.com>
2024-09-09 22:07:28 -0500

    Create Footer.vue

---
+++ EZ2RENO/EZ2RENO/src/components/Footer.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer>
+    <div class="p-5">
+      <div>
+        <img
+          src="/logo.png"
+          alt="Logo"
+          class="img-fluid me-3 mb-2 mb-md-0"
+          width="120"
+        />
+      </div>
+      <div class="py-4">
+        <ul class="d-flex gap-4 text-primary list-unstyled">
+          <li>
+            <a href="#" class="text-dark text-decoration-none">Privacy</a>
+          </li>
+          <li><a href="#" class="text-dark text-decoration-none">Legal</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="bg-blue-dark">
+      <p class="text-center text-light p-3">
+        © 2024 BuilderHubs. All Rights Reserved.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
