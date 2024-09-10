7b4bdb070a36a6503f61f51cd14e8c2c154e0bab
Hernando Abella <hernandoabella@gmail.com>
2024-09-09 22:07:18 -0500

    Create App.vue

---
+++ EZ2RENO/EZ2RENO/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Reputation from './components/Reputation.vue'
+import About from './components/About.vue'
+import Services from './components/Services.vue'
+import Booking from './components/Booking.vue'
+import CTA from './components/CTA.vue'
+import AboutUs from './components/AboutUs.vue'
+import Discover from './components/Discover.vue'
+import Contact from './components/Contact.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Header />
+  <Hero />
+  <Reputation />
+  <About />
+  <Services />
+  <Booking />
+  <CTA />
+  <AboutUs />
+  <Discover />
+  <Contact />
+  <Footer />
+</template>
+
+<style scoped>
+
+</style>
