06e82d6e53fdc8ab32d77530294b77eb9800d92c
Hernando Abella <hernandoabella@gmail.com>
2024-09-14 16:56:36 -0500

    Update Hero.vue

--- EZ2RENO/EZ2RENO/src/components/Hero.vue
+++ EZ2RENO/EZ2RENO/src/components/Hero.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts"></script>
 
 <template>
-  Hero
+  <div class="p-5 mb-5 bg-reno-hero">
+    <div class="p-md-5">
+      <div class="p-md-5">
+        <h1 class="fw-bold text-center">
+          Transform Your Home with Ease! efforless <br />
+          home renovation delivered by pros
+        </h1>
+        <p class="text-center">
+          Building quality homes for families just like yours. Get your bathroom
+          upgrades delivered to you by professionals. <br />
+          Add extra income by legal basements making it an effortless home
+          renovation for you.
+        </p>
+        <div class="d-flex m-auto justify-content-center">
+          <button class="fs-5 p-2 border-0 fw-bold btn bg-blue-dark text-light">
+            GET STARTED TODAY
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.bg-reno-hero {
+  background: url("/public/xavier-teo-ZNHgxie602M-unsplash.jpg");
+}
+</style>
