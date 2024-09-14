88768a09b1211523930f503356226adbe8e3f54f
Hernando Abella <hernandoabella@gmail.com>
2024-09-14 16:56:38 -0500

    Update Reputation.vue

--- EZ2RENO/EZ2RENO/src/components/Reputation.vue
+++ EZ2RENO/EZ2RENO/src/components/Reputation.vue
@@ -1,7 +1,34 @@
 <script setup lang="ts"></script>
 
 <template>
-  Reputation
+  <div class="col-md-6 mx-auto my-5 px-5">
+    <h2 class="text-center fw-bold mb-5">Our Reputation</h2>
+    <div class="row justify-content-center">
+      <div class="col-md-4 d-flex justify-content-center mb-4">
+        <div class="card shadow p-4 text-center">
+          <div class="my-4"><img src="/public/icon.png" alt="icon" /></div>
+          <h4 class="fw-bold">Best Services</h4>
+          <p>Nullam senectus porttitor in eget. Eget rutrum leo interdum.</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 d-flex justify-content-center mb-4">
+        <div class="card shadow p-4 text-center">
+          <div class="my-4"><img src="/public/icon.png" alt="icon" /></div>
+          <h4 class="fw-bold">Best Services</h4>
+          <p>Cursus semper tellus volutpat aliquet lacus.</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 d-flex justify-content-center mb-4">
+        <div class="card shadow p-4 text-center">
+          <div class="my-4"><img src="/public/icon-2.png" alt="icon" /></div>
+          <h4 class="fw-bold">Best Services</h4>
+          <p>Ultricies at ipsum nunc, tristique nam lectus.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
