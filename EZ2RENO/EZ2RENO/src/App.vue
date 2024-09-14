1b4dd274aa205ff352b18571433f60d64744d41a
Hernando Abella <hernandoabella@gmail.com>
2024-09-14 16:56:25 -0500

    Update App.vue

--- EZ2RENO/EZ2RENO/src/App.vue
+++ EZ2RENO/EZ2RENO/src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
-import Hero from './components/Hero.vue'
-import Reputation from './components/Reputation.vue'
-import About from './components/About.vue'
-import Services from './components/Services.vue'
-import Booking from './components/Booking.vue'
-import CTA from './components/CTA.vue'
-import AboutUs from './components/AboutUs.vue'
-import Discover from './components/Discover.vue'
-import Contact from './components/Contact.vue'
-import Footer from './components/Footer.vue'
+import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import Reputation from "./components/Reputation.vue";
+import About from "./components/About.vue";
+import Services from "./components/Services.vue";
+import Booking from "./components/Booking.vue";
+import CTA from "./components/CTA.vue";
+import AboutUs from "./components/AboutUs.vue";
+import Discover from "./components/Discover.vue";
+import Contact from "./components/Contact.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
@@ -26,6 +26,4 @@ import Footer from './components/Footer.vue'
   <Footer />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
