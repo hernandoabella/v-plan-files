01e6942a226084ca57745b08e01f33dd81bf157a
Hernando Abella <hernandoabella@gmail.com>
2024-09-14 16:56:33 -0500

    Update Discover.vue

--- EZ2RENO/EZ2RENO/src/components/Discover.vue
+++ EZ2RENO/EZ2RENO/src/components/Discover.vue
@@ -1,36 +1,43 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="p-5">
+  <div class="p-md-5 m-5">
     <h2 class="text-center fw-bolder">Discover Your Dream Home</h2>
     <p class="text-center">
-      At EZ2 Reno, our mission is to build homes that inspire and delight. Photo
-      Gallery.
+      At EZ2 Reno, our mission is to build homes that <br/> inspire and delight. Photo Gallery.
     </p>
 
-    <div>
-      <div class="w-100 d-flex w-100 d-flex justify-content-center">
-        <img src="/public/image 139.png" alt="kitchen" />
+    <div class="col-md-6 mx-auto">
+      <!-- Main Image -->
+      <div class="mb-4">
+        <img src="/public/image 139.png" alt="kitchen" class="img-fluid" />
       </div>
-      <div class="d-flex w-50 mx-auto justify-content-center gap-2 mt-2">
-        <div>
-          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+      <!-- Gallery Images -->
+      <div class="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-2 g-md-3">
+        <div class="col">
+          <img src="/public/image 139.png" alt="kitchen" class="img-fluid" />
         </div>
-        <div>
-          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        <div class="col">
+          <img src="/public/image 139.png" alt="kitchen" class="img-fluid" />
         </div>
-        <div>
-          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        <div class="col">
+          <img src="/public/image 139.png" alt="kitchen" class="img-fluid" />
         </div>
-        <div>
-          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        <div class="col">
+          <img src="/public/image 139.png" alt="kitchen" class="img-fluid" />
         </div>
-        <div>
-          <img src="/public/image 139.png" alt="kitchen" class="w-100" />
+        <div class="col">
+          <img src="/public/image 139.png" alt="kitchen" class="img-fluid" />
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Ensure images are responsive and scale appropriately */
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
+</style>
