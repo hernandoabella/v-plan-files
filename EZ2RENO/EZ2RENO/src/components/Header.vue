7df6701219594269f214b7b407fb8ddba74b33b1
Hernando Abella <hernandoabella@gmail.com>
2024-09-14 16:56:35 -0500

    Update Header.vue

--- EZ2RENO/EZ2RENO/src/components/Header.vue
+++ EZ2RENO/EZ2RENO/src/components/Header.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  Header goes here
+  <div>
+    <header>
+      <div class="d-flex justify-content-between px-5 py-3">
+        <div>
+          <div><img src="/logo.png" alt="logo" /></div>
+        </div>
+        <div class="d-flex justify-content-center align-items-center gap-4">
+          <div><img src="/typcn_user.png" alt="icon" /></div>
+          <div><img src="/ic_baseline-shopping-cart.png" alt="icon" /></div>
+        </div>
+      </div>
+    </header>
+  </div>
 </template>
 
 <style scoped></style>
